4b812ef9c2a56c5edc2f415d37b41ff5e0bd5274
Stefan kruger <stefan.kruger@gmail.com>
2019-12-02 14:18:33 +0000

    racket version, day2

--- day02.rkt
+++ day02.rkt
@@ -13,27 +13,18 @@
   (vector-set! state out (fn (vector-ref state i1) (vector-ref state i2)))
   state)
 
-(define (instr ip state)
-  (cond [(>= ip (- (vector-length state) 4)) (vector->list state)]
-        [else (list (vector-ref state ip) (vector-ref state (+ ip 1))
-                    (vector-ref state (+ ip 2)) (vector-ref state (+ ip 3)))]))
-
-(define (run data target noun verb)
+(define (run data noun verb)
   (define st (list->vector data))
   (vector-set! st 1 noun)
   (vector-set! st 2 verb)
-  (let loop ([ip 0] [state st])
-    (cond [(and (> target 0) (> (vector-ref state 0) target)) (vector-ref state 0)]
-          [else
-           (match (instr ip state)
-             [(list 1 in1 in2 out) (loop (+ ip 4) (opcode! state in1 in2 out +))]
-             [(list 2 in1 in2 out) (loop (+ ip 4) (opcode! state in1 in2 out *))]
-             [_ (vector-ref state 0)])])))
-             
+  (for/or ([instr (in-slice 4 st)])
+    (match instr
+      [(list 1 in1 in2 out) (opcode! st in1 in2 out +) #f]
+      [(list 2 in1 in2 out) (opcode! st in1 in2 out *) #f]
+      [_ (vector-ref st 0)])))
 
-(printf "Part1: ~a\n" (run data -1 12 2))
+(printf "Part1: ~a\n" (run data 12 2))
 (printf "Part2: ~a\n"
         (for*/first ([noun (in-range 100)]
-                     [verb (in-range 100)] #:when (= (run data 19690720 noun verb) 19690720))
+                     [verb (in-range 100)] #:when (= (run data noun verb) 19690720))
           (+ (* 100 noun) verb)))
-
