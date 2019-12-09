0f497d051fb762cf0fece3b7bdc76e92d1464d43
Stefan kruger <stefan.kruger@gmail.com>
2019-12-09 15:57:00 +0000

    day07 refactor + part2

--- day07.py
+++ day07.py
@@ -16,124 +16,129 @@ def read_data(filename="data/input07.data"):
         return (list(map(int, f.read().split(","))))
 
 
-CODE = read_data()
+class Amplifier:
+    def __init__(self, code):
+        self.input = []
+        self.output = []
+        self.terminated = False
+        self.data = code
+        self.ip = 0
+        self.opcodes = {
+            1: (self.add, 3),
+            2: (self.mul, 3),
+            3: (None, 1),
+            4: (None, 1),
+            5: (self.jt, 2),
+            6: (self.jf, 2),
+            7: (self.lt, 3),
+            8: (self.eq, 3),
+            99: (None, 0)
+        }
 
+    def parse_opcode(self, opcode):
+        if opcode < 10:
+            return [0] * self.opcodes[opcode][1], opcode
+        str_opc = str(opcode)
+        op = int(str_opc[-2:])
+        params = self.opcodes[op][1]
+        return list(reversed(list(map(int, list(str_opc[:-2].zfill(params)))))), op
 
-def read_string(data):
-    return (list(map(int, data.split(","))))
+    def value(self, mode, param):
+        if mode == 0:
+            return self.data[param]
+        return param
 
+    def add(self, modes):
+        params = self.data[self.ip+1:self.ip+4]
+        self.data[params[2]] = (
+            self.value(modes[0], params[0]) +
+            self.value(modes[1], params[1]))
+        self.ip += 4
 
-def add(data, ip, modes):
-    params = data[ip+1:ip+4]
-    data[params[2]] = (
-        value(data, modes[0], params[0]) +
-        value(data, modes[1], params[1]))
-    return ip + 4
+    def mul(self, modes):
+        params = self.data[self.ip+1:self.ip+4]
+        self.data[params[2]] = (
+            self.value(modes[0], params[0]) *
+            self.value(modes[1], params[1]))
+        self.ip += 4
 
+    def jt(self, modes):
+        params = self.data[self.ip+1:self.ip+3]
+        if self.value(modes[0], params[0]) != 0:
+            self.ip = self.value(modes[1], params[1])
+        else:
+            self.ip += 3
 
-def mul(data, ip, modes):
-    params = data[ip+1:ip+4]
-    data[params[2]] = (
-        value(data, modes[0], params[0]) *
-        value(data, modes[1], params[1]))
-    return ip + 4
+    def jf(self, modes):
+        params = self.data[self.ip+1:self.ip+3]
+        if self.value(modes[0], params[0]) == 0:
+            self.ip = self.value(modes[1], params[1])
+        else:
+            self.ip += 3
 
+    def lt(self, modes):
+        params = self.data[self.ip + 1:self.ip + 4]
+        self.data[params[2]] = int(
+            self.value(modes[0], params[0]) <
+            self.value(modes[1], params[1]))
+        self.ip += 4
 
-def jt(data, ip, modes):
-    params = data[ip+1:ip+3]
-    if value(data, modes[0], params[0]) != 0:
-        return value(data, modes[1], params[1])
-    return ip + 3
+    def eq(self, modes):
+        params = self.data[self.ip + 1:self.ip + 4]
+        self.data[params[2]] = int(
+            self.value(modes[0], params[0]) ==
+            self.value(modes[1], params[1]))
+        self.ip += 4
 
+    def add_input(self, d):
+        self.input.append(d)
+        self.run()
 
-def jf(data, ip, modes):
-    params = data[ip+1:ip+3]
-    if value(data, modes[0], params[0]) == 0:
-        return value(data, modes[1], params[1])
-    return ip + 3
-
-
-def lt(data, ip, modes):
-    params = data[ip + 1:ip + 4]
-    data[params[2]] = int(
-        value(data, modes[0], params[0]) <
-        value(data, modes[1], params[1]))
-    return ip + 4
-
-
-def eq(data, ip, modes):
-    params = data[ip + 1:ip + 4]
-    data[params[2]] = int(
-        value(data, modes[0], params[0]) ==
-        value(data, modes[1], params[1]))
-    return ip + 4
-
-
-OPCODES = {
-    1: (add, 3),
-    2: (mul, 3),
-    3: (None, 1),
-    4: (None, 1),
-    5: (jt, 2),
-    6: (jf, 2),
-    7: (lt, 3),
-    8: (eq, 3),
-    99: (None, 0)
-}
-
-
-def value(data, mode, param):
-    if mode == 0:
-        return data[param]
-    return param
-
-
-def parse_opcode(opcode):
-    if opcode < 10:
-        return [0] * OPCODES[opcode][1], opcode
-    str_opc = str(opcode)
-    op = int(str_opc[-2:])
-    params = OPCODES[op][1]
-    return list(reversed(list(map(int, list(str_opc[:-2].zfill(params)))))), op
-
-
-def run(phase_setting, input_data):
-    data = CODE.copy()
-    ip = 0
-    phase_consumed = False
-    while True:
-        modes, opc = parse_opcode(data[ip])
-        if opc == 3:
-            if not phase_consumed:
-                data[data[ip + 1]] = phase_setting
-                phase_consumed = True
+    def run(self):
+        while True:
+            modes, opc = self.parse_opcode(self.data[self.ip])
+            if opc == 3:
+                if len(self.input) == 0:
+                    return
+                self.data[self.data[self.ip + 1]] = self.input.pop(0)
+                self.ip += 2
+            elif opc == 4:
+                param = self.data[self.ip+1]
+                v = self.value(modes[0], param)
+                self.output.append(v)
+                self.ip += 2
+            elif opc == 99:
+                self.terminated = True
+                break
             else:
-                data[data[ip + 1]] = input_data
-            ip += 2
-            continue
-        elif opc == 4:
-            param = data[ip+1]
-            v = value(data, modes[0], param)
-            return v
-        if opc == 99:
-            break
-
-        ip = OPCODES[opc][0](data, ip, modes)
+                self.opcodes[opc][0](modes)
 
 
-def maximise():
-    best = None
-    for phase_setting in list(permutations(list(range(5)))):
-        input_data = 0
+def run_amplifiers(data, phases):
+    amps = [Amplifier(data.copy()) for _ in range(5)]
 
-        for phase in phase_setting:
-            result = run(phase, input_data)
-            input_data = result
+    for phase, amp in zip(phases, amps):
+        amp.add_input(phase)
 
-        if best is None or result > best:
-            best = result
-    return best
+    amps[0].add_input(0)
+
+    while not amps[4].terminated:
+        for i, amp in enumerate(amps):
+            amps[(i+1) % 5].add_input(amp.output[-1])
+
+    return amps[4].output[-1]
 
 
 if __name__ == "__main__":
-    print(f"Part1: {maximise()}")
+    d = read_data()
+    part1 = max(
+        run_amplifiers(d, phases)
+        for phases in permutations(range(5))
+    )
+
+    print(f"Part1: {part1}")
+    part2 = max(
+        run_amplifiers(d, phases)
+        for phases in permutations(range(5, 10)))
+    )
+    print(f"Part2: {part2}")
