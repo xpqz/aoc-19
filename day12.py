3f9d7ddce31bd8174d60848641577f62969678a8
Stefan kruger <stefan.kruger@gmail.com>
2019-12-12 17:11:33 +0000

    day12

--- day12.py
+++ day12.py
@@ -7,20 +7,16 @@
 #
 # By Stefan Kruger
 
-
 from dataclasses import dataclass
 from math import gcd
 import re
 from typing import Tuple
 
-
 def cmp(b, a):
     return (a > b) - (a < b)
 
-
 def lcm(x, y):
     return (x * y) // gcd(x, y)
-
 
 @dataclass(frozen=True)
 class Moon:
@@ -28,28 +24,23 @@ class Moon:
     vel: Tuple[int, int, int]
 
     def gravity(self, moon):
-        return tuple([cmp(self.pos[i], moon.pos[i]) for i in [0, 1, 2]])
+        return tuple([cmp(self.pos[i], moon.pos[i]) for i in range(3)])
 
     def apply_gravity(self, dv):
         p = list(self.pos)
         v = list(self.vel)
-        for i in [0, 1, 2]:
+        for i in range(3):
             v[i] += dv[i]
             p[i] += v[i]
         return Moon(pos=tuple(p), vel=tuple(v))
 
     def total_energy(self):
-        return (
-            sum([abs(i) for i in self.pos]) *
-            sum([abs(i) for i in self.vel])
-        )
-
+        return sum([abs(i) for i in self.pos]) * sum([abs(i) for i in self.vel])
 
 def make_moons(data):
     loc = [[int(s) for s in re.findall(r'-?\d+', l)] for l in data]
 
     return [Moon(pos=p, vel=(0, 0, 0))for p in loc]
-
 
 def apply_forces(moons):
     dv = []
@@ -65,13 +56,12 @@ def apply_forces(moons):
 
     return [m.apply_gravity(dv[i]) for i, m in enumerate(moons)]
 
-
 def find_periods(data):
     moons = make_moons(data)
     start_states = [tuple((m.pos[i] for m in moons)) for i in range(3)]
     periods = [-1, -1, -1]
     for i in range(1, 300_000):
-        states = [tuple((m.pos[i] for m in moons))for i in range(3)]
+        states = [tuple((m.pos[i] for m in moons)) for i in range(3)]
 
         if i > 1:
             for ii, st in enumerate(states):
@@ -82,21 +72,15 @@ def find_periods(data):
 
         moons = apply_forces(moons)
 
-
 def find_energy(data, iterations):
     moons = make_moons(data)
     for i in range(1, iterations+1):
         moons = apply_forces(moons)
     return sum([m.total_energy() for m in moons])
 
-
 if __name__ == "__main__":
-    data = [
-        "<x=-15, y=1, z=4>",
-        "<x=1, y=-10, z=-8>",
-        "<x=-5, y=4, z=9>",
-        "<x=4, y=6, z=-2>"
-    ]
+    data = ["<x=-15, y=1, z=4>", "<x=1, y=-10, z=-8>",
+            "<x=-5, y=4, z=9>", "<x=4, y=6, z=-2>"]
 
     print(f"Part1: {find_energy(data, 1000)}")
 
