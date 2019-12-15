b7ac17fbd8ad4f4c3971529a45fc32447d5ce1b2
Stefan kruger <stefan.kruger@gmail.com>
2019-12-15 11:46:10 +0000

    day13 p2

--- day13.py
+++ day13.py
@@ -7,7 +7,7 @@
 #
 # By Stefan Kruger
 
-from intcode import run
+from intcode import run, resume, decode
 from itertools import zip_longest
 
 
@@ -16,12 +16,44 @@ def read_data(filename="data/input13.data"):
         return f.read()
 
 
+def cmp(b, a):
+    return (a > b) - (a < b)
+
+
 def chunk(iterator, chunk_size):
     return zip_longest(*[iter(iterator)] * chunk_size)
 
 
+def play(program):
+    code = decode(program)
+    code[0] = 2
+    state = [code, 0, 0, [0], [], False]
+    score = 0
+    while state[5] is False:
+        state = resume(state)
+        ball = 0
+        paddle = 0
+        for item in chunk(state[4], 3):
+            if item[0] == -1 and item[1] == 0:
+                score = item[2]
+            elif item[2] == 4:
+                ball = item[0]
+            elif item[2] == 3:
+                paddle = item[0]
+
+        # just track the x-position of the ball
+        state[3].append(cmp(paddle, ball))
+        state[4] = []  # reset output buffer
+
+    return score
+
+
 if __name__ == "__main__":
     program = read_data()
-    out = run(program, lambda: 0)
+
+    out = run(program, lambda: 1)
+
     blocks = sum(1 for item in chunk(out, 3) if item[2] == 2)
-    print(blocks)
+    print(f"Part1: {blocks}")
+
+    print(f"Part2: {play(program)}")
---
+++ intcode.py
@@ -0,0 +1,84 @@
+from collections import defaultdict
+
+
+def decode(program):
+    return defaultdict(int, enumerate(map(int, program.split(','))))
+
+
+def run(program, input_callback, poke=[]):
+    ip = rb = 0
+    mem = decode(program)
+    for (addr, val) in poke:
+        mem[addr] = val
+
+    while True:
+        op = mem[ip] % 100
+        if op == 99:
+            return
+        size = [0, 4, 4, 2, 2, 3, 3, 4, 4, 2][op]
+        args = [mem[ip+i] for i in range(1, size)]
+        modes = [(mem[ip] // 10 ** i) % 10 for i in range(2, 5)]
+        reads = [(mem[x], x, mem[x+rb])[m] for x, m in zip(args, modes)]
+        writes = [(x, None, x+rb)[m] for x, m in zip(args, modes)]
+        ip += size
+        if op == 1:
+            mem[writes[2]] = reads[0] + reads[1]
+        elif op == 2:
+            mem[writes[2]] = reads[0] * reads[1]
+        elif op == 3:
+            mem[writes[0]] = input_callback()
+        elif op == 4:
+            yield reads[0]
+        elif op == 5 and reads[0]:
+            ip = reads[1]
+        elif op == 6 and not reads[0]:
+            ip = reads[1]
+        elif op == 7:
+            mem[writes[2]] = int(reads[0] < reads[1])
+        elif op == 8:
+            mem[writes[2]] = int(reads[0] == reads[1])
+        elif op == 9:
+            rb += reads[0]
+        elif op not in [1, 2, 3, 4, 5, 6, 7, 8, 9, 99]:
+            raise Exception(f"Unexpected opcode: {op}")
+
+
+def resume(state):
+    # state = [mem, ip, rb, input, output, terminated]
+
+    while True:
+        op = state[0][state[1]] % 100
+        if op == 99:
+            state[5] = True
+            return state
+        size = [0, 4, 4, 2, 2, 3, 3, 4, 4, 2][op]
+        args = [state[0][state[1]+i] for i in range(1, size)]
+        modes = [(state[0][state[1]] // 10 ** i) % 10 for i in range(2, 5)]
+        reads = [(state[0][x], x, state[0][x+state[2]])[m] for x, m in zip(args, modes)]
+        writes = [(x, None, x + state[2])[m] for x, m in zip(args, modes)]
+
+        state[1] += size
+
+        if op == 1:
+            state[0][writes[2]] = reads[0] + reads[1]
+        elif op == 2:
+            state[0][writes[2]] = reads[0] * reads[1]
+        elif op == 3:
+            if not state[3]:
+                state[1] -= size   # underrun, reverse one step
+                return state
+            state[0][writes[0]] = state[3].pop(0)
+        elif op == 4:
+            state[4].append(reads[0])
+        elif op == 5 and reads[0]:
+            state[1] = reads[1]
+        elif op == 6 and not reads[0]:
+            state[1] = reads[1]
+        elif op == 7:
+            state[0][writes[2]] = int(reads[0] < reads[1])
+        elif op == 8:
+            state[0][writes[2]] = int(reads[0] == reads[1])
+        elif op == 9:
+            state[2] += reads[0]
+        elif op not in [1, 2, 3, 4, 5, 6, 7, 8, 9, 99]:
+            raise Exception(f"Unexpected opcode: {op}")
